9f70b36eafa33e7b4a7b717fdd225f96f91da44e
eukap <esk.geek@gmail.com>
2019-01-29 21:17:20 +0200

    Basic arithmetic operations added

--- calc.py
+++ calc.py
@@ -1,68 +1,148 @@
 ''' This is a simple calculator '''
 
-import math
-import decimal
+from decimal import Decimal
 
-s1 = ''
-s2 = ''
-sign = ('+', '-', '*', '/')
+from arithm import mult_div, add_subtr
 
-s = input()
 
-for i in s:
-    if (str.isdigit(i) or i in sign):
-        s1 += i
-    elif i == ',':
-        s1 += '.'
-    else:
-        print('"' + i + '"', 'is not a digit!')
+while True:
+
+    sign = ('+', '-', '*', '/')
+    s = ''
+
+    try:
+        s0 = input('-> ')
+    except:
+        print()
         break
 
-count = -1
-for i in s1:
-    count += 1
-    if s1[0] in sign[2:len(sign)]:
-        print('Incorrect expression!')
-        break
-    elif s1[0] in sign[0:2]:
-        if (s1[1] in sign):
-            print('Incorrect expression!')
+    if not s0 or str.isspace(s0):
+        continue
+    elif '\t' in s0:
+        print('Tabulation is not supported')
+        continue
+
+    error = False
+    for i in range(len(s0)):
+        if i < len(s0) - 1 and s0[i] == ' ' and s0[i + 1] == ' ':
+            error = True
             break
-    elif (s1[-1] in sign or s1[-1] == '.'):
-        print('Incorrect expression!')
-        break
-    elif i in sign:
-        if s1[count + 1] in sign[2:len(sign)]:
-            print('Incorrect expression!')
-            break
-        elif s1[count + 1] in sign[0:2]:
-            if (s1[count + 2] in sign):
-                print('Incorrect expression!')
+        if (str.isdigit(s0[i]) or s0[i] == '.') and i < len(s0) - 2:
+            if (s0[i + 1] == ' ' and
+               (str.isdigit(s0[i + 2]) or s0[i + 2] == '.')):
+                error = True
                 break
-    elif i == '.':
-        if (s1[count + 1] in sign or s1[count + 1] == '.'):
-            print('Incorrect expression!')
+    if error:
+        print('Incorrect expression')
+        continue
+
+    s0 = s0.replace(' ', '')
+
+    if s0[0] in sign[2:] or s0[-1] in sign:
+        print('Incorrect expression')
+        continue
+    elif s0[0] in sign[:2]:
+        if len(s0) > 1 and s0[1] in sign:
+            print('Incorrect expression')
+            continue
+
+    signs_count = 0
+    digits = error = False
+    for x in s0:
+        if str.isdigit(x):
+            digits = True
+            s += x
+        elif x in sign:
+            signs_count += 1
+            s += x
+        elif x == '.':
+            s += x
+        else:
+            error = True
             break
+    if error or not digits:
+        print('Incorrect expression')
+        continue
+    elif signs_count == 0:
+        print(s)
+        continue
+    elif signs_count == 1:
+        if s[0] == '-':
+            print(s)
+            continue
+        elif s[0] == '+':
+            print(s[1:])
+            continue
 
-result = decimal.Decimal(0)
-# An operand is a string at first:
-op = ''
-count = -1
-for i in s1:
-    count += 1
-    if s1[0] == '+':
-        if (count + 1 < len(s1)):
-            if (str.isdigit(s1[count + 1]) or s1[count + 1] == '.'):
-                op += s1[count + 1]
-                continue        
-        result = result + decimal.Decimal(op)
+    error = False
+    for i in range(len(s)):
+        if s[i] in sign and s[i + 1] in sign:
+            if s[i + 1] in sign[2:]:
+                error = True
+                break
+            elif s[i + 2] in sign:
+                error = True
+                break
+    if error:
+        print('Incorrect expression')
+        continue
 
-    if s1[0] == '-':
-        if (count + 1 < len(s1)):
-            if (str.isdigit(s1[count + 1]) or s1[count + 1] == '.'):
-                op += s1[count + 1]
-                continue        
-        result = result - decimal.Decimal(op)
+    s = s.replace('++', '+')
+    s = s.replace('+-', '-')
+    s = s.replace('-+', '-')
+    s = s.replace('--', '+')
+    s = s.replace('*+', '*')
+    s = s.replace('/+', '/')
 
-print(result)
+    buf1 = ''
+    for i in range(len(s)):
+        if s[i] == '+' or s[i] == '*' or s[i] == '/':
+            buf1 += ' '
+        elif s[i] == '-' and s[i - 1] not in sign:
+                buf1 += ' '
+        else:
+            buf1 += s[i]
+
+    if buf1[0] == ' ':
+        buf1 = s[0] + buf1[1:]
+
+    buf1 = buf1.split()
+
+    try:
+        for x in buf1:
+            Decimal(x)
+    except:
+        print('Incorrect expression')
+        continue
+
+    buf2 = ''
+    for i in range(len(s)):
+        if str.isdigit(s[i]) or s[i] == '.':
+            buf2 += ' '
+        elif s[i] == '-' and s[i -1] in sign:
+            buf2 += ' '
+        else:
+            buf2 += s[i]
+
+    if s[0] in sign:
+        buf2 = buf2[1:]
+
+    buf2 = buf2.split()
+
+    I = iter(buf1)
+    for i in range(0, (len(buf1) + (len(buf2))), 2):
+        buf2.insert(i, next(I))
+
+    if '*' in buf2 or '/' in buf2:
+        buf2 = mult_div(buf2)
+        if buf2 == False:
+            continue
+
+    if '+' in buf2 or '-' in buf2:
+        buf2 = add_subtr(buf2)
+
+    buf2 = buf2[0]
+
+    print(buf2)
+    continue
 
